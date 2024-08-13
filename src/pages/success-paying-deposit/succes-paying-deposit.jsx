4f43dc3d1e5e28a75ee8e21d5ae4a3f3fa529f95
Fectel <HectorGamador@gmail.com>
2024-08-13 15:21:23 -0700

    Update succes-paying-deposit.jsx

--- src/pages/success-paying-deposit/succes-paying-deposit.jsx
+++ src/pages/success-paying-deposit/succes-paying-deposit.jsx
@@ -30,9 +30,20 @@ const SuccessPayingDeposit = () => {
     console.log("Succds page", params)
     // console.log("Succds page", params)
 
+    
 
     function checkClientWSConnection(){
 
+        socket.on("connect_error", (err) => {
+  // the reason of the error, for example "xhr poll error"
+  console.log(err.message);
+
+  // some additional description, for example the status code of the initial HTTP response
+  console.log(err.description);
+
+  // some additional context, for example the XMLHttpRequest object
+  console.log(err.context);
+});
         console.log("checkClientWSConnection")
         client.onopen = () => {
             console.log('Websocket CLiecnt Connected')
