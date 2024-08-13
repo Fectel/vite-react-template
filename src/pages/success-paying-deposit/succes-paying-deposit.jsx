e944b02298389688ff2182c265aa51a6e0894456
Fectel <HectorGamador@gmail.com>
2024-08-12 18:43:07 -0700

    Update succes-paying-deposit.jsx ip

--- src/pages/success-paying-deposit/succes-paying-deposit.jsx
+++ src/pages/success-paying-deposit/succes-paying-deposit.jsx
@@ -17,7 +17,7 @@ import { useParams } from 'react-router-dom';
 
 const SuccessPayingDeposit = () => {
     console.log("Succds page")
-    const client = new W3CWebSocket('https://badassmariachi.com/ws');
+    const client = new W3CWebSocket('https://badassmariachi.com/');
 
 
     const [url , setUrl ] = useState("")
