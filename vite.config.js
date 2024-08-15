0bf7e24e420c33f34921e578e1df748c94f8fd39
Fectel <HectorGamador@gmail.com>
2024-08-15 15:46:39 +0000

    edite vite config.js

--- vite.config.js
+++ vite.config.js
@@ -4,24 +4,24 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
 
-  server: {
-    port: 3000,
-    proxy: {
-      '/api': {
-        target:"https://triumphant-beauty-production.up.railway.app",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\api/, ''),
-      },
+  // server: {
+    // port: 3000,
+    // proxy: {
+    //   '/api': {
+    //     target:"https://triumphant-beauty-production.up.railway.app",
+    //     changeOrigin: true,
+    //     rewrite: (path) => path.replace(/^\api/, ''),
+    //   },
       // '/socket.io': {
       //   target:"https://badassmariachi.com",
       //   changeOrigin: true,
         // rewrite: (path) => path.replace(/^\socekt.io/, ''),
       // }
-    },
+    // },
     // hmr: {
     //   host: "https://triumphant-beauty-production.up.railway.app/webhook",
     //   protocol: "wss",
     // },
-  },
+  // },
   plugins: [react()],
 })
