ef91d3459f94a53186c37c8c3a133b77cdaae822
Fectel <HectorGamador@gmail.com>
2024-08-14 20:50:08 -0700

    Update succes-paying-deposit.jsx commented out client.on

--- src/pages/success-paying-deposit/succes-paying-deposit.jsx
+++ src/pages/success-paying-deposit/succes-paying-deposit.jsx
@@ -44,42 +44,42 @@ const SuccessPayingDeposit = () => {
   // some additional context, for example the XMLHttpRequest object
   console.log(err.context);
 });
-        console.log("checkClientWSConnection")
-        client.onopen = () => {
-            console.log('Websocket CLiecnt Connected')
-        }
+        // console.log("checkClientWSConnection")
+        // client.onopen = () => {
+        //     console.log('Websocket CLiecnt Connected')
+        // }
 
-        client.onmessage = async (message) => {
-            console.log("REceived a message@const", message.data)
-            // const dataFromServer = JSON.parse(message.data);
-            // console.log('got reply! ', dataFromServer)
-            // messagesArr.push(message.data)
-            console.log(message.data)
-            const msgArr = message.data.split("@")
-            console.log(msgArr)
-            setUrl(msgArr[0])
-            setAmount(msgArr[1]/100)
-            console.log(params)
+        // client.onmessage = async (message) => {
+        //     console.log("REceived a message@const", message.data)
+        //     // const dataFromServer = JSON.parse(message.data);
+        //     // console.log('got reply! ', dataFromServer)
+        //     // messagesArr.push(message.data)
+        //     console.log(message.data)
+        //     const msgArr = message.data.split("@")
+        //     console.log(msgArr)
+        //     setUrl(msgArr[0])
+        //     setAmount(msgArr[1]/100)
+        //     console.log(params)
 
-            await updateBookingStatusToReserved(params.clientId,params.contractId, msgArr[0], msgArr[4]).then(r =>
-            {
-                //also add the link to download the pdf
-                // and include tha link in the client dashboard bookings component
-                window.location.href = '/client-dashboard'
-            })
-            // await saveNewMembership(msgArr[1], msgArr[0], msgArr[2], msgArr[3])
+        //     await updateBookingStatusToReserved(params.clientId,params.contractId, msgArr[0], msgArr[4]).then(r =>
+        //     {
+        //         //also add the link to download the pdf
+        //         // and include tha link in the client dashboard bookings component
+        //         window.location.href = '/client-dashboard'
+        //     })
+        //     // await saveNewMembership(msgArr[1], msgArr[0], msgArr[2], msgArr[3])
 
-            // await saveNewMembershipSubscriptionTransaction(msgArr[1], msgArr[4],msgArr[5]).then((res) => {
-            //    console.log(res)
-            //     if (res === true){
-            //         navigate("/sign-in-dashboard-page")
-            //
-            //     }else{
-            //         window.prompt("Error Saving Transaction")
-            //     }
-            // })
-        }
-        console.log("checkClientWSConnection", client)
+        //     // await saveNewMembershipSubscriptionTransaction(msgArr[1], msgArr[4],msgArr[5]).then((res) => {
+        //     //    console.log(res)
+        //     //     if (res === true){
+        //     //         navigate("/sign-in-dashboard-page")
+        //     //
+        //     //     }else{
+        //     //         window.prompt("Error Saving Transaction")
+        //     //     }
+        //     // })
+        // }
+        // console.log("checkClientWSConnection", client)
 
     }
 
