68e7976f66d88ea94afdedf2904d4fd762bf4def
Fectel <HectorGamador@gmail.com>
2024-08-16 14:09:13 +0000

    chanmged socket

--- src/pages/success-paying-deposit/succes-paying-deposit.jsx
+++ src/pages/success-paying-deposit/succes-paying-deposit.jsx
@@ -14,8 +14,8 @@ import { useParams } from 'react-router-dom';
 
 
 const SuccessPayingDeposit = () => {
-    // const socket = io.connect('https://badassmariachi.com/'
-    const socket = io.connect('https://mariachihero.com/:2096'
+    const socket = io.connect('https://badassmariachi.com/'
+    // const socket = io.connect('https://mariachihero.com/:2096'
         , {transports: ['websocket'],}, 
                              )
 
